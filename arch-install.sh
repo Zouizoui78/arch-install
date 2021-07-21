f3e8177ba63743347ec383587415100ceddb54ea
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2021-07-21 23:30:07 +0200

    Use sed instead of calling vim, need testing

--- arch-install.sh
+++ arch-install.sh
@@ -23,26 +23,23 @@ function yesno
     echo $ask
 }
 
-pacman -S vim --noconfirm
-
-#Enable multilib and color option
-vim /etc/pacman.conf
+# Enable multilib and color option
+sed -i "s/#Color/Color/" /etc/pacman.conf
+sed -i "s/#[multilib]\n#Include/[multilib]\nInclude/" /etc/pacman.conf
 pacman -Syu --noconfirm
 
-#Enable multicore for make compilation
-vim /etc/makepkg.conf
+# Use all available cores for makepkg compilation
+sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j'`nproc`'"/' /etc/makepkg.conf
 
 ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 hwclock --systohc
 
-#Activate en_DK.UTF-8 locale
-#en_DK to have english with normal measurement units and formats for time, date, etc
+# Activate en_DK.UTF-8 locale
+# en_DK to have english with normal measurement units and formats for time, date, etc
 echo "en_DK.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 
-#en_DK : english with normal formats for everything
 echo "LANG=en_DK.UTF-8" > /etc/locale.conf
-# echo "KEYMAP=fr-latin1" > /etc/vconsole.conf
 
 echo "Hostname ?"
 read hostname
@@ -63,17 +60,17 @@ cpu=$(numchoice 1 2)
 echo "Did you install the linux-lts kernel ? y/n"
 lts=$(yesno)
 
-echo "Choose which desktop to install :
+echo "Select a desktop environment :
 1- Gnome
 2- KDE"
 de=$(numchoice 1 2)
 
-packages="dkms wpa_supplicant dhcpcd grub efibootmgr os-prober ntfs-3g openssh base-devel python git man-db man-pages texinfo ncdu htop nmap unrar unzip i7z nss-mdns pacman-contrib rsync wget inetutils vlc qbittorrent libreoffice-still libreoffice-still-fr cups noto-fonts-emoji pulseaudio pulseaudio-alsa discord pinta nvidia-utils lib32-nvidia-utils vulkan-icd-loader lib32-vulkan-icd-loader steam lutris wine wine-mono wine-gecko"
+packages="dkms wpa_supplicant dhcpcd grub efibootmgr os-prober ntfs-3g openssh base-devel python vim git man-db man-pages texinfo ncdu htop nmap unrar unzip i7z nss-mdns pacman-contrib rsync wget inetutils vlc qbittorrent libreoffice-still libreoffice-still-fr cups noto-fonts-emoji pulseaudio pulseaudio-alsa discord pinta nvidia-dkms nvidia-utils lib32-nvidia-utils vulkan-icd-loader lib32-vulkan-icd-loader steam lutris wine wine-mono wine-gecko"
 
 if [ $lts = "y" ]; then
-    packages="${packages} linux-lts-headers nvidia-lts"
+    packages="${packages} linux-lts-headers"
 else
-    packages="${packages} linux-headers nvidia"
+    packages="${packages} linux-headers"
 fi
 
 case $cpu in
@@ -83,13 +80,20 @@ esac
 
 case $de in
     1) packages="${packages} baobab eog evince file-roller gdm gedit gnome-boxes gnome-calculator gnome-characters gnome-control-center gnome-disk-utility gnome-keyring gnome-logs gnome-menus gnome-remote-desktop gnome-screenshot gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-themes-extra gnome-user-docs gnome-video-effects gvfs gvfs-goa mutter nautilus networkmanager simple-scan tracker3 tracker3-miners xdg-user-dirs-gtk gnome-sound-recorder gnome-tweaks";;
-    2) packages="${packages} xorg breeze breeze-gtk drkonqi kactivitymanagerd kde-cli-tools kde-gtk-config kdecoration kdeplasma-addons kgamma5 khotkeys kinfocenter kmenuedit kscreen kscreenlocker ksysguard kwallet-pam kwayland-integration kwayland-server kwin kwrited libkscreen libksysguard milou plasma-browser-integration plasma-desktop plasma-disks plasma-firewall plasma-integration plasma-nm plasma-pa plasma-systemmonitor plasma-workspace polkit-kde-agent powerdevil sddm-kcm systemsettings xdg-desktop-portal-kde skanlite ark dolphin gwenview kate kcalc kfind kompare konsole krdc krfb ksystemlog kwalletmanager okular partitionmanager spectacle kdialog yakuake sddm";;
+    2) packages="${packages} breeze breeze-gtk drkonqi kactivitymanagerd kde-cli-tools kde-gtk-config kdecoration kdeplasma-addons kgamma5 khotkeys kinfocenter kmenuedit kscreen kscreenlocker ksysguard kwallet-pam kwayland-integration kwayland-server kwin kwrited libkscreen libksysguard milou plasma-browser-integration plasma-desktop plasma-disks plasma-firewall plasma-integration plasma-nm plasma-pa plasma-systemmonitor plasma-workspace polkit-kde-agent powerdevil sddm-kcm systemsettings xdg-desktop-portal-kde skanlite ark dolphin gwenview kate kcalc kfind kompare konsole krdc krfb ksystemlog kwalletmanager okular partitionmanager spectacle kdialog yakuake sddm";;
 esac
 
 pacman -S --needed $packages
 
-#Give root commands access to wheel group
-vim /etc/sudoers
+# Give root commands access to wheel group
+echo "Do you want to use sudo without password ? y/n"
+sudo=$(yesno)
+if [ $sudo = "y" ]; then
+    sed -i 's/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+else
+    packages="${packages} linux-headers"
+    sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+fi
 
 echo "Username ?"
 read username
@@ -97,15 +101,21 @@ useradd -m $username --groups wheel,adm,ftp,games,http,log,rfkill,sys,audio,scan
 echo "$username password"
 passwd $username
 
-#Change pgp server for one that is more reliable than the default one.
+# Change pgp server for one that is more reliable than the default one.
 mkdir -p /home/${username}/.gnupg
 chown -R ${username}:${username} /home/${username}/.gnupg
 chmod -R u+rwX,g-rwx,o-rwx /home/${username}/.gnupg
 echo "keyserver hkps://keyserver.ubuntu.com" >> /home/${username}/.gnupg/dirmngr.conf
 
 grub-install --target=x86_64-efi --efi-directory=/EFI --bootloader-id=arch
-#Set grub settings
-vim /etc/default/grub
+
+# Set grub settings
+sed -i 's/GRUB_DEFAULT=0/GRUB_DEFAULT=saved/' /etc/default/grub
+sed -i 's/ quiet//' /etc/default/grub
+sed -i 's/#GRUB_SAVEDEFAULT=true/GRUB_SAVEDEFAULT=true/' /etc/default/grub
+echo '# Enable os prober
+GRUB_DISABLE_OS_PROBER=false' >> /etc/default/grub
+
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable cups NetworkManager fstrim.timer avahi-daemon systemd-timesyncd
